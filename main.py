b6e21853766365b3f27ef9fd01cfc8518814e878
mkruiz <m.ruizm@unal.edu.co>
2023-06-25 20:50:51 -0500

    Actualización

--- main.py
+++ main.py
@@ -7,13 +7,13 @@ class ApiInput(BaseModel):
     texts: List[str]
 
 class ApiOutput(BaseModel):
-    is_hate: List[int]
+    is_false: List[int]
 
 app = FastAPI() # creamos el api
 model = joblib.load("model_final.joblib") # cargamos el modelo.
 
-@app.post("/hate") # creamos api que permita requests de tipo post.
+@app.post("/false") # creamos api que permita requests de tipo post.
 async def create_user(data: ApiInput) -> ApiOutput:
     predictions = model.predict(data.texts).flatten().tolist() # generamos la predicción
-    preds = ApiOutput(is_hate=predictions) # estructuramos la salida del API.
+    preds = ApiOutput(is_false=predictions) # estructuramos la salida del API.
     return preds # retornamos los resultados
